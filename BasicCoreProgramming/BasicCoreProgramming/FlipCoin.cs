34e52729af3d2a5ecc02c5519dc242b899f787d8
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-05-14 21:10:12 +0530

    To Check Leap Year or Not

--- BasicCoreProgramming/BasicCoreProgramming/FlipCoin.cs
+++ BasicCoreProgramming/BasicCoreProgramming/FlipCoin.cs
@@ -8,6 +8,42 @@ namespace BasicCoreProgramming
 {
     public class FlipCoin
     {
+        public void CheckFlipCoin()
+        {
+            int Result;
+            int Heads = 0;      // Taking Variable 
+            int Tails = 0;
+            Random CoinFLip = new Random();
 
+            // Taking Input
+            Console.Write("Enter The Number of Times to Flip Coin:");
+            int NoOfFlip = Convert.ToInt32(Console.ReadLine());
+
+
+
+            // Defining Logic
+            for (int i = 0; i < NoOfFlip; i++)
+            {
+                Result = CoinFLip.Next(0, 2);
+                if (Result < 0.5)
+                {
+                    Tails++;
+                }
+                else
+                {
+                    Heads++;
+
+                }
+            }
+            Console.WriteLine("Number of Head : {0}", Heads);
+            Console.WriteLine("Number of Tails : {0}", Tails);
+
+            float PerOfHeads = (float)Heads / (float)NoOfFlip * 100;
+            float PerOfTails = (float)Tails / (float)NoOfFlip * 100;
+
+            // printing Output 
+            Console.WriteLine("Percantange of Head is {0}% & Percantage of Tails is {1}", PerOfHeads, PerOfTails);
+
+        }
     }
 }
--- BasicCoreProgramming/BasicCoreProgramming/Program.cs
+++ BasicCoreProgramming/BasicCoreProgramming/Program.cs
@@ -10,8 +10,36 @@ namespace BasicCoreProgramming
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to  Basic of C# Core Programming ");
+            Console.WriteLine("***** Welcome to  Basic of C# Core Programming *****");
+            Console.WriteLine("\n");
+            Console.WriteLine("Enter The Choice of Question");
+            Console.WriteLine("Q1. FlipCoin & to print Percentege of Heads & Tails");
+            Console.WriteLine("Q2. To print Leap Year or Not a Leap Year");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+            // Creating Oject of FlipCoin Class
+                    FlipCoin objflipcoin = new FlipCoin();
+                    
+
+            // Calling The Method or Funtion
+                    objflipcoin.CheckFlipCoin();
+                    break;
+                case 2:
+                    LeapYear objleapyear = new LeapYear();
+                    objleapyear.CheckLeapYear();
+                    break;
+             }
+
+            //To Hold The Output Terminal 
             Console.ReadKey();
+
+
         }
+           
+
+            
+        
     }
 }
